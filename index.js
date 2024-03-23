2b41f89eeeb9ee19cfdbf56eee905a3afc9d8baa
Akuivan13-github <riskiivan132@gmail.com>
2024-03-23 22:23:05 +0700

    iaaka

--- index.js
+++ index.js
@@ -5,36 +5,19 @@ const helloRoute = require("./routes/helloRoute");
 const tiktokRoute = require("./routes/tiktokRoute");
 const igstalkRoute = require("./routes/igStalk");
 const aiRoute = require("./routes/aiRoute");
-
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 // Middleware untuk mem-parsing JSON bodies
 app.use(express.json());
 
-// CSS kustom untuk mengubah tampilan Swagger UI
-const customCss = `
-    .swagger-ui .topbar .link {
-        display: none;
-    }
-    .swagger-ui .topbar:before {
-        content: "Akuivan13 (Request Fitur Wa 089505520763)";
-        display: block;
-        font-weight: bold;
-        color: black;
-        font-size: 20px;
-        margin: 15px 0;
-        text-align: left;
-        padding-left: 80px;
-    }
-    .swagger-ui .topbar {
-        background: url('https://telegra.ph/file/727ec9ce1a059ca515074.jpg') no-repeat;
-        background-size: contain;
-    }
-`;
+// Serve Swagger documentation dengan Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Serve Swagger documentation beserta CSS kustom
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCss }));
+// Serve halaman utama
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 // Routes
 app.use("/", helloRoute);
@@ -42,12 +25,7 @@ app.use("/", tiktokRoute);
 app.use("/", igstalkRoute);
 app.use("/", aiRoute);
 
-// Route untuk halaman utama
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server sedang berjalan, mendengarkan port ${PORT}`);
+    console.log(`Server sedang berjalan, mendengarkan port ${PORT}`);
 });
