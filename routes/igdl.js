a2e292bc72a0831e8c73d56a64e98ccb7b2e34f7
Akuivan13-github <riskiivan132@gmail.com>
2024-03-30 21:16:01 +0700

    fix igdl

--- routes/igdl.js
+++ routes/igdl.js
@@ -25,46 +25,44 @@ router.get("/api/igdl", async (req, res) => {
   }
 });
 
-async function igdl(url) {
-  let res = await axios("https://indown.io/");
-  let _$ = cheerio.load(res.data);
-  let referer = _$("input[name=referer]").val();
-  let locale = _$("input[name=locale]").val();
-  let _token = _$("input[name=_token]").val();
-  let { data } = await axios.post(
-    "https://indown.io/download",
-    new URLSearchParams({
-      link: url,
-      referer,
-      locale,
-      _token,
-    }),
-    {
-      headers: {
-        cookie: res.headers["set-cookie"].join("; "),
-      },
-    }
-  );
-  let $ = cheerio.load(data);
-  let result = [];
-  let __$ = cheerio.load($("#result").html());
-  __$("video").each(function () {
-    let $$ = $(this);
-    result.push({
-      type: "video",
-      thumbnail: $$.attr("poster"),
-      url: $$.find("source").attr("src"),
-    });
-  });
-  __$("img").each(function () {
-    let $$ = $(this);
-    result.push({
-      type: "image",
-      url: $$.attr("src"),
-    });
-  });
 
-  return result;
+async function igdl(url) {
+  return new Promise(async (resolve, reject) => {
+    const payload = new URLSearchParams(
+      Object.entries({
+        url: url,
+        host: "instagram"
+      })
+    )
+    await axios.request({
+      method: "POST",
+      baseURL: "https://saveinsta.io/core/ajax.php",
+      data: payload,
+      headers: {
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        cookie: "PHPSESSID=rmer1p00mtkqv64ai0pa429d4o",
+        "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36"
+      }
+    })
+    .then(( response ) => {      
+      const $ = cheerio.load(response.data)
+      const mediaURL = $("div.row > div.col-md-12 > div.row.story-container.mt-4.pb-4.border-bottom").map((_, el) => {
+        return "https://saveinsta.io/" + $(el).find("div.col-md-8.mx-auto > a").attr("href")
+      }).get()
+      const res = {
+        status: 200,
+        media: mediaURL
+      }
+      resolve(res)
+    })
+    .catch((e) => {
+      console.log(e)
+      throw {
+        status: 400,
+        message: "error",
+      }
+    })
+  })
 }
 
 module.exports = router;
